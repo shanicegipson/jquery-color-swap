caf450d4472d2cbb4af94d5a0b55911b2a607e50
Shanice Gipson <shanicegipson13@gmail.com>
2019-08-05 13:47:10 -0500

    add red background

--- client.js
+++ client.js
@@ -1,24 +1,26 @@
 $(document).ready(readyNow);
 
-let element = 1;
+let element = 0;
 
 function readyNow() {
     $('.js-btn-generate').on('click', addElement);
     $('.js-container').on('click', '.js-btn-yellow', changeYellow);
     $('.js-container').on('click', '.js-btn-delete',  deleteElement);
+    clickCounter();
 }
 
 function addElement (){
     $('.js-container').append(`
         <div>
-            <p>${element}</p>
+            <p>${element}
             <button class="js-btn-yellow">Yellow</button>
             <button class="js-btn-delete">DELETE</button>
+            </p>
         </div>`);
 }
 
 function clickCounter (){
-    console.log('counter');
+    element++;
 }
 
 function changeYellow(){
