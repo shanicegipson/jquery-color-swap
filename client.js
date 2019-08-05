fc4c86f4235ffca9c1f26e2af11cd6732c2dfd9d
Shanice Gipson <shanicegipson13@gmail.com>
2019-08-05 13:42:00 -0500

    added button functionality

--- client.js
+++ client.js
@@ -1,5 +1,30 @@
 $(document).ready(readyNow);
 
+let element = 1;
+
 function readyNow() {
-    console.log('in ready');
+    $('.js-btn-generate').on('click', addElement);
+    $('.js-container').on('click', '.js-btn-yellow', changeYellow);
+    $('.js-container').on('click', '.js-btn-delete',  deleteElement);
+}
+
+function addElement (){
+    $('.js-container').append(`
+        <div>
+            <p>${element}</p>
+            <button class="js-btn-yellow">Yellow</button>
+            <button class="js-btn-delete">DELETE</button>
+        </div>`);
+}
+
+function clickCounter (){
+    console.log('counter');
+}
+
+function changeYellow(){
+    console.log ('in Yellow');
+}
+
+function deleteElement () {
+    console.log('in delete');
 }
